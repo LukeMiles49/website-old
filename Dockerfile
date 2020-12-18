92466f5e6eceb0153ba5573f7cfeb80b7240a1e4
Luke Miles <luke.49@live.co.uk>
2020-12-18 19:58:28 +0000

    Simple hello world website on a Raspberry Pi

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM rustlang/rust:nightly-alpine AS build
+FROM arm64v8/rust:slim AS build
 WORKDIR /usr/local/src/website
 COPY . .
 RUN cargo build --release
 
-FROM alpine:latest
+FROM debian:buster-slim
 WORKDIR /usr/local/bin
 COPY --from=build /usr/local/src/website/target/release/website ./website
 EXPOSE 80
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::net::{TcpListener};
+use std::io::{Write};
+
 fn main() {
-	println!("Hello, world!");
+	let listener = TcpListener::bind("localhost:80").unwrap();
+	
+	println!("Hello, Docker!");
+	
+	for stream in listener.incoming() {
+		println!("Someone connected :O");
+		
+		let contents = "Hello, World!";
+		
+		let response = format!(
+			"HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+			contents.len(),
+			contents,
+		);
+		
+		let mut stream = stream.unwrap();
+		stream.write(response.as_bytes()).unwrap();
+		stream.flush().unwrap();
+	}
 }
